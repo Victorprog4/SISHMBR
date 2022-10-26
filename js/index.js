4ecb83c15992fad32e8fff30b4c061b9a3f08f33
Victorprog4 <victroeeee4862@gmail.com>
2022-10-26 14:42:59 -0300

    first commit

---
+++ js/index.js
@@ -0,0 +1,8 @@
+var swiper = new Swiper(".swiper", {
+    cssMode: true,
+    loop: true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
