31f27db214edb79bc6c4eb3752744a5612ca3ebd
Victorprog4 <victroeeee4862@gmail.com>
2022-11-29 15:48:06 -0300

    versao 1.1

--- js/index.js
+++ js/index.js
@@ -1,8 +1,25 @@
-var swiper = new Swiper(".swiper", {
-    cssMode: true,
-    loop: true,
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-  });
+var swiper = new Swiper(".swiper-home", {
+  cssMode: true,
+  loop: true,
+  autoplay: {
+    delay: 2000,
+  },
+});
+
+var swiper = new Swiper(".mySwiper", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  coverflowEffect: {
+    rotate: 40,
+    stretch: 0,
+    depth: 100,
+    modifier: 1,
+    slideShadows: false,
+  },
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+});
